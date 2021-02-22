bb59d0d8532a636d38cd3cc6953fb8e58bc893e7
FleeingSunlight <gh@fleeingsunlight.dev>
2021-02-22 09:27:39 +0700

    feat(migrations/user): update

--- migrations/20210222091947_user.ts
+++ migrations/20210222091947_user.ts
@@ -1,10 +1,18 @@
 import * as Knex from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable("user", (table) => {
+    table.bigIncrements("id").primary();
+    table.text("first_name").notNullable();
+    table.text("last_name").notNullable();
+    table.text("email").notNullable().unique();
+    table.text("nickname");
+    table.text("chula_id");
+    table.boolean("is_chula_student").notNullable();
+    table.enum("gender", ["M", "F", "NS"]).notNullable();
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists("user");
 }
-
