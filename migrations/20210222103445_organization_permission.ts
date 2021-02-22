4b48d0cb3d3a1d03a88b6fab342b21acaefbd1c7
Patthanat Thanintantrakun <boomeevee@icloud.com>
2021-02-22 12:17:50 +0700

    feat(organization_permission): add content

--- migrations/20210222103445_organization_permission.ts
+++ migrations/20210222103445_organization_permission.ts
@@ -1,10 +1,19 @@
-import * as Knex from "knex";
-
+import * as Knex from "knex"
 
 export async function up(knex: Knex): Promise<void> {
-}
+  await knex.schema.createTable("organization_permission", (table) => {
+    table.bigIncrements("id").primary()
 
+    table.bigInteger("organization_id").notNullable()
+    table
+      .foreign("organization_id")
+      .references("organization.id")
+      .onDelete("CASCADE")
+
+    table.text("permission").notNullable()
+  })
+}
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists("organization_permission")
 }
-
