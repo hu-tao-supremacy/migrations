7c793529b7cc08bffbacdd5e75685ba34d5b0b36
Patthanat Thanintantrakun <boomeevee@icloud.com>
2021-02-22 12:30:27 +0700

    feat(facility_request): add content

--- migrations/20210222103432_facility_request.ts
+++ migrations/20210222103432_facility_request.ts
@@ -1,10 +1,23 @@
-import * as Knex from "knex";
-
+import * as Knex from "knex"
 
 export async function up(knex: Knex): Promise<void> {
-}
+  await knex.schema.createTable("facility_request", (table) => {
+    table.bigIncrements("id").primary()
 
+    table.bigInteger("event_id").notNullable()
+    table.foreign("event_id").references("event.id").onDelete("CASCADE")
+
+    table.bigInteger("facility_id").notNullable()
+    table.foreign("facility_id").references("facility.id").onDelete("CASCADE")
+
+    table.enum("status", ["PENDING", "APPROVED", "REJECTED"]).notNullable()
+
+    table.text("reject_reason")
+    table.timestamp("start", { useTz: true }).notNullable()
+    table.timestamp("end", { useTz: true }).notNullable()
+  })
+}
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists("facility_request")
 }
-
