70498912317e288c555d3226cc5a4d082634c194
FleeingSunlight <gh@fleeingsunlight.dev>
2021-02-22 09:21:07 +0700

    feat(migrations): add migration for `user` table

--- knexfile.ts
+++ knexfile.ts
@@ -1,44 +1,14 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./dev.sqlite3"
-    }
-  },
-
-  staging: {
     client: "postgresql",
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
+      host: "localhost",
+      database: "hts",
+      user: "hu-tao-mains",
+      password: "hu-tao-mains",
+      port: 5432,
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
   },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  }
-
 };
