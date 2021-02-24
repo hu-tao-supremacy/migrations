c28785041319c2129f0d33827c1c93e69472b5f0
Patthanat Thanintantrakun <boomeevee@icloud.com>
2021-02-24 16:17:17 +0700

    refractor(event)

--- seeds/staging/event.ts
+++ seeds/staging/event.ts
@@ -1,17 +1,20 @@
-import * as Knex from "knex";
-import * as faker from "faker";
+import * as Knex from "knex"
+import * as faker from "faker"
 
 export async function seed(knex: Knex): Promise<void> {
-    const data = new Array(200).fill(undefined).map((_) => {
-        return {
-            organization_id: faker.random.number({ min: 1, max: 100 }),
-            event_location_id: faker.random.number({ min: 1, max: 200 }),
-            description: faker.lorem.paragraph(),
-            name: faker.lorem.sentence(),
-            contact: faker.lorem.text()
-        };
-    });
+  // Deletes ALL existing entries
+  await knex("event").del()
 
-    // Inserts seed entries
-    await knex("event").insert(data);
+  const data = new Array(200).fill(undefined).map((_) => {
+    return {
+      organization_id: faker.random.number({ min: 1, max: 100 }),
+      event_location_id: faker.random.number({ min: 1, max: 200 }),
+      description: faker.lorem.paragraph(),
+      name: faker.lorem.sentence(),
+      contact: faker.lorem.text(),
+    }
+  })
+
+  // Inserts seed entries
+  await knex("event").insert(data)
 }
