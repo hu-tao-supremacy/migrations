394aa57c888cdafb3b3ad96865684c4dc332693c
Patthanat Thanintantrakun <boomeevee@icloud.com>
2021-02-24 16:10:52 +0700

    refractor(organization)

--- seeds/staging/organization.ts
+++ seeds/staging/organization.ts
@@ -1,17 +1,17 @@
-import * as Knex from "knex";
-import * as faker from "faker";
+import * as Knex from "knex"
+import * as faker from "faker"
 
 export async function seed(knex: Knex): Promise<void> {
-    // Deletes ALL existing entries
-    await knex("organization").del();
+  // Deletes ALL existing entries
+  await knex("organization").del()
 
-    const data = new Array(100).fill(undefined).map((_) => {
-        return {
-            name: faker.company.companyName(),
-            is_verified: faker.random.boolean(),
-        };
-    });
+  const data = new Array(100).fill(undefined).map((_) => {
+    return {
+      name: faker.company.companyName(),
+      is_verified: faker.random.boolean(),
+    }
+  })
 
-    // Inserts seed entries
-    await knex("organization").insert(data);
+  // Inserts seed entries
+  await knex("organization").insert(data)
 }
