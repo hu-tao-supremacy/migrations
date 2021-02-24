97a2ae7a8db15d7eef372877f5384fef17d482b0
Patthanat Thanintantrakun <boomeevee@icloud.com>
2021-02-24 16:18:18 +0700

    fix(facility): add del()

--- seeds/staging/facility.ts
+++ seeds/staging/facility.ts
@@ -1,28 +1,31 @@
-import * as Knex from "knex";
-import * as faker from "faker";
+import * as Knex from "knex"
+import * as faker from "faker"
 
 export async function seed(knex: Knex): Promise<void> {
-    const data = new Array(200).fill(undefined).map((_) => {
-        const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-        const d = faker.random.arrayElements(days);
-        const a = d.map((day) => {
-            return {
-                day,
-                start: "8.00",
-                finish: "16.30",
-            };
-        });
+  // Deletes ALL existing entries
+  await knex("facility").del()
 
-        return {
-            organization_id: faker.random.number({ min: 1, max: 100 }),
-            name: faker.company.companyName(),
-            latitude: faker.address.latitude(),
-            longitude: faker.address.longitude(),
-            operating_hours: JSON.stringify(a),
-            description: faker.lorem.paragraph(),
-        };
-    });
+  const data = new Array(200).fill(undefined).map((_) => {
+    const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+    const d = faker.random.arrayElements(days)
+    const a = d.map((day) => {
+      return {
+        day,
+        start: "8.00",
+        finish: "16.30",
+      }
+    })
 
-    // Inserts seed entries
-    await knex("facility").insert(data);
+    return {
+      organization_id: faker.random.number({ min: 1, max: 100 }),
+      name: faker.company.companyName(),
+      latitude: faker.address.latitude(),
+      longitude: faker.address.longitude(),
+      operating_hours: JSON.stringify(a),
+      description: faker.lorem.paragraph(),
+    }
+  })
+
+  // Inserts seed entries
+  await knex("facility").insert(data)
 }
