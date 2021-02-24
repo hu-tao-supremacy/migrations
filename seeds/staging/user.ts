8cc3e8b2246245fb3fa07e4df670eb8fce94e5fa
FleeingSunlight <gh@fleeingsunlight.dev>
2021-02-24 09:48:38 +0700

    feat(seeds/staging): add user.ts

--- seeds/staging/user.ts
+++ seeds/staging/user.ts
@@ -1,13 +1,23 @@
 import * as Knex from "knex";
+import * as faker from "faker";
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
     await knex("user").del();
 
+    const data = new Array(100).fill(undefined).map((_) => {
+        const isStudent = faker.random.boolean();
+        return {
+            first_name: faker.name.firstName,
+            last_name: faker.name.lastName,
+            email: faker.internet.email,
+            nickname: faker.random.boolean() ? faker.name.firstName : null,
+            chula_id: isStudent ? faker.random.number(9999999999) : null,
+            is_chula_student: isStudent,
+            gender: faker.random.arrayElement(["M", "F", "NS"]),
+        };
+    });
+
     // Inserts seed entries
-    await knex("user").insert([
-        { id: 1, colName: "rowValue1" },
-        { id: 2, colName: "rowValue2" },
-        { id: 3, colName: "rowValue3" },
-    ]);
+    await knex("user").insert(data);
 }
