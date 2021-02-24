e1cbf7b291e76b8dd9690ee03eb677e4477314a8
FleeingSunlight <gh@fleeingsunlight.dev>
2021-02-24 10:07:15 +0700

    feat(seeds/staging): init user-organization.ts

--- seeds/staging/user_organization.ts
+++ seeds/staging/user_organization.ts
@@ -1,13 +1,37 @@
 import * as Knex from "knex";
+import * as faker from "faker";
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
-    await knex("table_name").del();
+    await knex("user_organization").del();
+
+    const a = new Array(100).fill(undefined).map((_, i) => {
+        const index = i + 1;
+        return {
+            user_id: index,
+            organization_id: index,
+        };
+    });
+
+    const b: { user_id: number; organization_id: number }[] = new Array(100)
+        .fill(undefined)
+        .map((_) => {
+            const user = faker.random.number({ min: 1, max: 100 });
+            const organization = faker.random.number({ min: 1, max: 100 });
+
+            if (user !== organization) {
+                return {
+                    user_id: user,
+                    organization_id: organization,
+                };
+            }
+
+            return undefined;
+        })
+        .filter((data) => data);
+
+    const data = a.concat(b);
 
     // Inserts seed entries
-    await knex("table_name").insert([
-        { id: 1, colName: "rowValue1" },
-        { id: 2, colName: "rowValue2" },
-        { id: 3, colName: "rowValue3" }
-    ]);
-};
+    await knex("user_organization").insert(data);
+}
