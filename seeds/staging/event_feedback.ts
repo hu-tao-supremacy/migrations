63102b7030aac198fcf3a1cecb94e81e26b00b5e
Patthanat Thanintantrakun <boomeevee@icloud.com>
2021-02-24 17:39:42 +0700

    fix event_id max

--- seeds/staging/event_feedback.ts
+++ seeds/staging/event_feedback.ts
@@ -4,7 +4,7 @@ import faker from "faker"
 export async function seed(knex: Knex): Promise<void> {
   const data = new Array(200).fill(undefined).map((_) => {
     return {
-      event_id: faker.random.number({ min: 1, max: 100 }),
+      event_id: faker.random.number({ min: 1, max: 200 }),
       feedback: faker.lorem.paragraph(),
     }
   })
