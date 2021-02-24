f4562bce6bba81c0ed1e022754765fafa92f5ff6
FleeingSunlight <gh@fleeingsunlight.dev>
2021-02-24 17:16:36 +0700

    feat(seeds/staging): update event_duration.ts

--- seeds/staging/event_duration.ts
+++ seeds/staging/event_duration.ts
@@ -1,24 +1,18 @@
-import * as Knex from "knex"
-import * as faker from "faker"
+import * as Knex from "knex";
+import * as faker from "faker";
+import { DateTime } from "luxon";
 
 export async function seed(knex: Knex): Promise<void> {
-  const randomDate = (start: Date, end: Date) => {
-    return new Date(
-      start.getTime() + Math.random() * (end.getTime() - start.getTime())
-    )
-  }
-
-  const date1 = randomDate(new Date(2020, 1, 1), new Date(2025, 12, 1))
-  const date2 = date1.setDate(
-    date1.getDate() + faker.random.number({ min: 100, max: 10000 })
-  )
-
-  const data = new Array(200).fill(undefined).map((_) => {
+  const data = new Array(500).fill(undefined).map((_) => {
+    let now = DateTime.now().setZone("Asia/Bangkok");
+    let isPast = faker.random.boolean();
+    let start = isPast ? now.minus({ months: 5 }) : now.plus({ months: 1 });
+    let end = start.plus({ hours: faker.random.number({ min: 1, max: 3 }) });
     return {
       event_id: faker.random.number({ min: 1, max: 200 }),
-      start: date1,
-      end: date2,
-    }
-  })
-  await knex("event_duration").insert(data)
+      start: start.toISO(),
+      end: end.toISO(),
+    };
+  });
+  await knex("event_duration").insert(data);
 }
