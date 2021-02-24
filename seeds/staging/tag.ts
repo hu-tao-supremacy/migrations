391a3865d7ecc88b8e4d1902e83ae972cf54338c
FleeingSunlight <gh@fleeingsunlight.dev>
2021-02-24 17:08:33 +0700

    fix(seeds/staging): unique tag.ts

--- seeds/staging/tag.ts
+++ seeds/staging/tag.ts
@@ -1,13 +1,19 @@
-import * as Knex from "knex"
-import * as faker from "faker"
+import * as Knex from "knex";
+import * as faker from "faker";
 
 export async function seed(knex: Knex): Promise<void> {
-  const data = new Array(200).fill(undefined).map((_) => {
+  let set = new Set();
+  while (set.size < 200) {
+    set.add(faker.random.word());
+  }
+
+  let words = Array.from(set);
+  const data = new Array(200).fill(undefined).map((_, index) => {
     return {
-      name: faker.random.word(),
-    }
-  })
+      name: words[index],
+    };
+  });
 
   // Inserts seed entries
-  await knex("tag").insert(data)
+  await knex("tag").insert(data);
 }
