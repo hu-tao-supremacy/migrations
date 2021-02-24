a22532e74c32110621897ade25b96bd2b0455fd8
Patthanat Thanintantrakun <boomeevee@icloud.com>
2021-02-24 16:13:12 +0700

    fix(event_organization): add del()

--- seeds/staging/event_location.ts
+++ seeds/staging/event_location.ts
@@ -1,17 +1,18 @@
-import * as Knex from "knex";
-import * as faker from "faker";
+import * as Knex from "knex"
+import * as faker from "faker"
 
 export async function seed(knex: Knex): Promise<void> {
-    // Deletes ALL existing entries
+  // Deletes ALL existing entries
+  await knex("event_organization").del()
 
-    const data = new Array(200).fill(undefined).map((_) => {
-        return {
-            name: faker.address.state(),
-            latitude: faker.address.latitude(),
-            longitude: faker.address.longitude(),
-        };
-    });
+  const data = new Array(200).fill(undefined).map((_) => {
+    return {
+      name: faker.address.state(),
+      latitude: faker.address.latitude(),
+      longitude: faker.address.longitude(),
+    }
+  })
 
-    // Inserts seed entries
-    await knex("event_location").insert(data);
+  // Inserts seed entries
+  await knex("event_location").insert(data)
 }
