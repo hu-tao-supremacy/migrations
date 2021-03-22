f7f5b5b7950eb9fb2c698b4d585af0cd98421087
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-22 11:22:07 +0700

    feat(migrations/user-permission): add `permission_name`

--- migrations/20210222103454_user_permission.ts
+++ migrations/20210222103454_user_permission.ts
@@ -1,20 +1,16 @@
-import * as Knex from "knex"
+import * as Knex from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable("user_permission", (table) => {
-    table.bigIncrements("id").primary()
+    table.bigIncrements("id").primary();
 
-    table.bigInteger("user_id").notNullable()
-    table.foreign("user_id").references("user.id").onDelete("CASCADE")
+    table.bigInteger("user_id").notNullable();
+    table.foreign("user_id").references("user.id").onDelete("CASCADE");
 
-    table.bigInteger("organization_permission_id").notNullable()
-    table
-      .foreign("organization_permission_id")
-      .references("organization_permission.id")
-      .onDelete("CASCADE")
-  })
+    table.text("permission_name").notNullable();
+  });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTableIfExists("user_permission")
+  await knex.schema.dropTableIfExists("user_permission");
 }
