6a9490c0b8fea948dcdb0cc78db8659763145f92
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-23 21:35:09 +0700

    feat(factories/tag): `name` must be unique

--- factories/tag.factory.ts
+++ factories/tag.factory.ts
@@ -3,6 +3,6 @@ import { Tag } from "../entities/tag.entity";
 
 define(Tag, (faker) => {
   const tag = new Tag();
-  tag.name = faker.lorem.word(1);
+  tag.name = `${faker.lorem.word(1)} - ${faker.random.alphaNumeric(6)}`;
   return tag;
 });
