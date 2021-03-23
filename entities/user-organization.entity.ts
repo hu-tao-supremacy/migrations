0376d0c5090d09534ca775393dc7520f098b740f
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-23 16:26:05 +0700

    feat(entities/user-org): add unique indices

--- entities/user-organization.entity.ts
+++ entities/user-organization.entity.ts
@@ -1,7 +1,14 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  Index,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 import { Organization } from "./organization.entity";
 import { User } from "./user.entity";
 
+@Index(["userId", "organizationId"], { unique: true })
 @Entity()
 export class UserOrganization {
   @PrimaryGeneratedColumn()
@@ -10,12 +17,12 @@ export class UserOrganization {
   @Column()
   userId: number;
 
-  @ManyToOne(() => User)
+  @ManyToOne(() => User, { onDelete: "CASCADE" })
   user: User;
 
   @Column()
   organizationId: number;
 
-  @ManyToOne(() => Organization)
+  @ManyToOne(() => Organization, { onDelete: "CASCADE" })
   organization: Organization;
 }
