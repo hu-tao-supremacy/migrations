4fe672322a151437f3e48e009af70a5f3087b93f
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-23 14:34:09 +0700

    feat(entities/user-interest): add relations to other entities

--- entities/user-interest.entity.ts
+++ entities/user-interest.entity.ts
@@ -1,4 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Tag } from "./tag.entity";
+import { User } from "./user.entity";
 
 @Entity()
 export class UserInterest {
@@ -8,6 +10,12 @@ export class UserInterest {
   @Column()
   userId: number;
 
+  @ManyToOne(() => User)
+  user: User;
+
   @Column()
   tagId: number;
+
+  @ManyToOne(() => Tag)
+  tag: Tag;
 }
