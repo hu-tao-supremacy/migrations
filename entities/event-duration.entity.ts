5005fba0b7e7bc7d4d3b5ac9c715aa1659288e1a
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-23 13:39:22 +0700

    feat(entities/event-duration): add relations to other entities

--- entities/event-duration.entity.ts
+++ entities/event-duration.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Event } from "./event.entity";
 
 @Entity()
 export class EventDuration {
@@ -13,4 +14,7 @@ export class EventDuration {
 
   @Column("timestamptz")
   finish: string;
+
+  @ManyToOne(() => Event)
+  event: Event;
 }
