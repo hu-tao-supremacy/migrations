90b6aa7ff54396b718bce0a07bd2178a074c3814
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-23 16:10:21 +0700

    feat(entities/question-group): add unique indices

--- entities/question-group.entity.ts
+++ entities/question-group.entity.ts
@@ -1,6 +1,14 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  Index,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 import { Event } from "./event.entity";
 
+@Index(["eventId", "type"], { unique: true })
+@Index(["eventId", "order"], { unique: true })
 @Entity()
 export class QuestionGroup {
   @PrimaryGeneratedColumn()
@@ -9,7 +17,7 @@ export class QuestionGroup {
   @Column()
   eventId: number;
 
-  @ManyToOne(() => Event)
+  @ManyToOne(() => Event, { onDelete: "CASCADE" })
   event: Event;
 
   @Column("enum", { enum: ["PRE_EVENT", "POST_EVENT"] })
