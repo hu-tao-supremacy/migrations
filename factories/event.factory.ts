5890ded1ad4dd7f0225941f53c3288a3a628243f
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-23 22:09:19 +0700

    feat(factories/event): add toTitleCase()

--- factories/event.factory.ts
+++ factories/event.factory.ts
@@ -2,11 +2,12 @@ import { define, factory } from "typeorm-seeding";
 import { Event } from "../entities/event.entity";
 import { Organization } from "../entities/organization.entity";
 import { Location } from "../entities/location.entity";
+import toTitleCase from "to-title-case";
 
 define(Event, (faker) => {
   const hasLocation = faker.random.boolean();
   let event = new Event();
-  event.name = faker.lorem.words(6);
+  event.name = toTitleCase(faker.lorem.words(6));
   event.description = faker.lorem.paragraph();
   event.organization = factory(Organization)() as any;
   event.location = hasLocation ? (factory(Location)() as any) : null;
