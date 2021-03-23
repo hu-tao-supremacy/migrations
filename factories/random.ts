332413b7ee812df35746d3df7a27db986d9686b2
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-23 22:06:42 +0700

    fix(factories/random.ts): predicate -> predicates

--- factories/random.ts
+++ factories/random.ts
@@ -3,7 +3,7 @@ import friendlyWords from "friendly-words";
 import toTitleCase from "to-title-case";
 
 export function wordGroup(): string {
-  return [friendlyWords.predicate, friendlyWords.objects, friendlyWords.teams]
+  return [friendlyWords.predicates, friendlyWords.objects, friendlyWords.teams]
     .map((list) => toTitleCase(faker.random.arrayElement(list)))
     .join(" ") as string;
 }
