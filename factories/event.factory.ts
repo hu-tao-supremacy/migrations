7ba094463af246fe98bbe8130ae368ae4d852c04
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-23 21:13:24 +0700

    feat(factories/event): add `event.location`

--- factories/event.factory.ts
+++ factories/event.factory.ts
@@ -3,10 +3,12 @@ import { Event } from "../entities/event.entity";
 import { Organization } from "../entities/organization.entity";
 
 define(Event, (faker) => {
+  const hasLocation = faker.random.boolean();
   let event = new Event();
   event.name = faker.lorem.words(6);
   event.description = faker.lorem.paragraph();
   event.organization = factory(Organization)() as any;
+  event.location = hasLocation ? (factory(Location)() as any) : null;
   event.attendeeLimit = faker.random.number(50);
   return event;
 });
