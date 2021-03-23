54685dd5a958bd07c8dccc6a2a341e5a07cabb62
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-23 22:51:53 +0700

    feat(seeds/e2e): update

--- seeds/e2e/e2e.seed.ts
+++ seeds/e2e/e2e.seed.ts
@@ -1,9 +1,28 @@
 import { Connection } from "typeorm";
 import { Seeder, Factory } from "typeorm-seeding";
+import { EventTag } from "../../entities/event-tag.entity";
 import { UserEvent } from "../../entities/user-event.entity";
+import faker from "faker";
+import { UserOrganization } from "../../entities/user-organization.entity";
 
 export default class E2E implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<any> {
-    await factory(UserEvent)().create();
+    const userEventArray = await factory(UserEvent)().createMany(200);
+    const userArray = userEventArray.map((userEvent) => userEvent.user);
+    const eventArray = userEventArray.map((userEvent) => userEvent.event);
+    const organizationArray = eventArray.map((event) => event.organization);
+    const eventTagArray = await factory(EventTag)()
+      .map(async (eventTag: EventTag) => {
+        eventTag.event = faker.random.arrayElement(eventArray);
+        return eventTag;
+      })
+      .createMany(50);
+    const userOrganizationArray = await factory(UserOrganization)()
+      .map(async (userOrg: UserOrganization) => {
+        userOrg.user = faker.random.arrayElement(userArray);
+        userOrg.organization = faker.random.arrayElement(organizationArray);
+        return userOrg;
+      })
+      .createMany(50);
   }
 }
