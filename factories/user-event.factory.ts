3a21b44b336ec776cadf231c1477248bb20533f4
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-23 21:06:21 +0700

    feat(factories/user-event): :tada:

---
+++ factories/user-event.factory.ts
@@ -0,0 +1,17 @@
+import { define, factory } from "typeorm-seeding";
+import { Event } from "../entities/event.entity";
+import { UserEvent } from "../entities/user-event.entity";
+import { User } from "../entities/user.entity";
+
+define(UserEvent, (faker) => {
+  const isApproved = faker.random.boolean();
+  const userEvent = new UserEvent();
+  userEvent.event = factory(Event)() as any;
+  userEvent.user = factory(User)() as any;
+  userEvent.status = isApproved
+    ? "APPROVED"
+    : faker.random.arrayElement(["PENDING", "REJECTED"]);
+  userEvent.ticket = isApproved ? faker.random.alphaNumeric(6) : null;
+  userEvent.rating = isApproved ? faker.random.number(5) : null;
+  return userEvent;
+});
