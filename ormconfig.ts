e6794afe08cd41fac2aa8a7e4bd99f787d99de0c
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-23 14:50:22 +0700

    feat(ormconfig.ts): :tada:

---
+++ ormconfig.ts
@@ -0,0 +1,9 @@
+export default {
+  type: "postgres",
+  host: "localhost",
+  database: "hts",
+  username: "hu-tao-mains",
+  password: "hu-tao-mains",
+  port: "5432",
+  entities: ["entities/*.ts"],
+};
