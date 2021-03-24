53d384bb5705112168b36d229548a97942cfc421
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-24 11:50:46 +0700

    feat(factories/question-group): :tada:

---
+++ factories/question-group.factory.ts
@@ -0,0 +1,11 @@
+import { define, factory } from "typeorm-seeding";
+import { QuestionGroup } from "../entities/question-group.entity";
+import { wordGroup } from "./random";
+import { Event } from "../entities/event.entity";
+define(QuestionGroup, (faker) => {
+  let questionGroup = new QuestionGroup();
+  questionGroup.type = faker.random.arrayElement(["PRE_EVENT", "POST_EVENT"]);
+  questionGroup.title = wordGroup();
+  questionGroup.event = factory(Event)() as any;
+  return questionGroup;
+});
