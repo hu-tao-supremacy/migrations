57c0e1f3ed1a323a63f065e736ec52609a8218e9
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-24 11:53:19 +0700

    feat(factories/question): :tada:

---
+++ factories/question.factory.ts
@@ -0,0 +1,14 @@
+import { define, factory } from "typeorm-seeding";
+import { QuestionGroup } from "../entities/question-group.entity";
+import { Question } from "../entities/question.entity";
+import { wordGroup } from "./random";
+
+define(Question, (faker) => {
+  let question = new Question();
+  question.title = `${wordGroup}?`;
+  question.subtitle = faker.lorem.sentence();
+  question.answerType = faker.random.arrayElement(["SCALE", "TEXT"]);
+  question.isOptional = faker.random.boolean();
+  question.questionGroup = factory(QuestionGroup)() as any;
+  return question;
+});
