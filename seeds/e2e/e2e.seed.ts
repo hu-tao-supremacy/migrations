6465c337bcd92945333f4b1a07ff386d219f1121
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-24 14:49:24 +0700

    feat(seeds/e2e): add QuestionGroup + Question

--- seeds/e2e/e2e.seed.ts
+++ seeds/e2e/e2e.seed.ts
@@ -5,6 +5,10 @@ import { UserEvent } from "../../entities/user-event.entity";
 import faker from "faker";
 import { UserOrganization } from "../../entities/user-organization.entity";
 import { EventDuration } from "../../entities/event-duration.entity";
+import { QuestionGroup } from "../../entities/question-group.entity";
+import { Event } from "../../entities/event.entity";
+import _ from "lodash";
+import { Question } from "../../entities/question.entity";
 
 export default class E2E implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<any> {
@@ -31,5 +35,51 @@ export default class E2E implements Seeder {
         return eventDuration;
       })
       .createMany(50);
+
+    {
+      const questionGroupOptions: any = {};
+      questionGroupOptions.n = 5;
+      questionGroupOptions.idx = 0;
+
+      const questionGroupMapping: { event: Event; order: number }[] = _.flatMap(
+        eventArray.map((event) => {
+          return _.range(1, questionGroupOptions.n + 1).map((n) => {
+            return { event, order: n };
+          });
+        })
+      );
+
+      const questionGroupArray = await factory(QuestionGroup)()
+        .map(async (group: QuestionGroup) => {
+          group.event = questionGroupMapping[questionGroupOptions.idx].event;
+          group.order = questionGroupMapping[questionGroupOptions.idx].order;
+          group.type = "PRE_EVENT";
+          questionGroupOptions.idx++;
+          return group;
+        })
+        .createMany(eventArray.length * questionGroupOptions.n);
+
+      const questionOptions: any = {};
+      questionOptions.n = 3;
+      questionOptions.idx = 0;
+
+      const questionMapping: { groupId: number; order: number }[] = _.flatMap(
+        questionGroupArray.map((group) => {
+          return _.range(1, questionOptions.n + 1).map((n) => {
+            return { groupId: group.id, order: n };
+          });
+        })
+      );
+
+      const questionArray = await factory(Question)()
+        .map(async (question: Question) => {
+          question.questionGroupId =
+            questionMapping[questionOptions.idx].groupId;
+          question.order = questionMapping[questionOptions.idx].order;
+          questionOptions.idx++;
+          return question;
+        })
+        .createMany(questionGroupArray.length * questionOptions.n);
+    }
   }
 }
